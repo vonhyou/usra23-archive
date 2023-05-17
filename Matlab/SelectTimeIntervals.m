1ef0b8e7699e6b61e85569c12fc7ed07a6cdbcb3
vonhyou <moi@rfung.dev>
2023-05-17 18:18:38 -0300

    add counting data

--- Matlab/SelectTimeIntervals.m
+++ Matlab/SelectTimeIntervals.m
@@ -9,7 +9,7 @@ maxTime = 5 * 60;
 lambda = 1 / 25;
 
 % Preallocate the browsing times array
-maxWebsites = floor(totalTime / minTime);
+maxWebsites = ceil(totalTime / minTime);
 browsingTimes = zeros(1, maxWebsites);
 
 % Generate the browsing times
@@ -25,12 +25,8 @@ end
 browsingTimes(i:end) = [];
 
 % Calculate the total number of counts and the total time spent
-totalCounts = length(browsingTimes);
-totalTimeSpent = sum(browsingTimes);
-
-% Display the total number of counts and the total time spent
-fprintf('Total number of websites visited: %d\n', totalCounts);
-fprintf('Total time spent: %d seconds\n', totalTimeSpent);
+totalCounts = length(browsingTimes) - 1;
+totalTimeSpent = sum(browsingTimes) - browsingTimes(end);
 
 % Draw a histogram of the browsing times with 30 bins
 figure;
@@ -44,6 +40,10 @@ subplot(2,1,2);
 bar(unique(browsingTimes), histcounts(browsingTimes, [unique(browsingTimes) maxTime + 1]));
 xlabel('Time (s)');
 ylabel('Count');
+
+% Add the results to the bottom of the figure
+str = sprintf('Total number of websites visited: %d\nDuration: %d seconds', totalCounts, totalTimeSpent);
+annotation('textbox', [0.46, 0.32, 0.8, 0.1], 'String', str, 'FitBoxToText', 'on');
 
 % Function to draw from a truncated exponential distribution
 function x = truncatedExponentialRandom(lambda, min, max)
