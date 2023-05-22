0b3d773dd34de074ee7dce4b4ea916d4f954a234
vonhyou <moi@rfung.dev>
2023-05-22 19:12:03 -0300

    auto swipe

---
+++ Scripts/auto_swipe.ps1
@@ -0,0 +1,27 @@
+# Ask the user for the total duration for the script to run (in minutes)
+$scriptDurationInMinutes = Read-Host -Prompt 'Enter the duration for the script to run (in minutes)'
+
+# Confirm the user input
+$confirmation = Read-Host -Prompt "You have entered $scriptDurationInMinutes minutes. Is this correct? (yes/no)"
+
+# If the user confirms, start the script
+if ($confirmation -eq 'yes') {
+    # Calculate the end time
+    $endTime = (Get-Date).AddMinutes($scriptDurationInMinutes)
+
+    while((Get-Date) -lt $endTime) {
+        # Generate a random delay between 0.5 and 3 minutes
+        $randomDelayInMinutes = Get-Random -Minimum 0.5 -Maximum 3
+
+        # Execute the adb command
+        adb shell input swipe 700 2300 700 1000
+
+        # Convert the random delay to milliseconds (1 minute = 60000 milliseconds)
+        $delayInMilliseconds = $randomDelayInMinutes * 60000
+
+        # Wait for the specified delay before repeating the loop
+        Start-Sleep -Milliseconds $delayInMilliseconds
+    }
+} else {
+    Write-Host "Please run the script again and enter the correct duration."
+}
