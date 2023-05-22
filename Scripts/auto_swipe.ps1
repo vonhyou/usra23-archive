d90b9d1e18ac65a62a02235e528b6d0493acfd6f
vonhyou <moi@rfung.dev>
2023-05-22 19:15:34 -0300

    err handling

--- Scripts/auto_swipe.ps1
+++ Scripts/auto_swipe.ps1
@@ -4,24 +4,34 @@ $scriptDurationInMinutes = Read-Host -Prompt 'Enter the duration for the script 
 # Confirm the user input
 $confirmation = Read-Host -Prompt "You have entered $scriptDurationInMinutes minutes. Is this correct? (yes/no)"
 
+# Convert the confirmation to lowercase
+$confirmation = $confirmation.ToLower()
+
 # If the user confirms, start the script
 if ($confirmation -eq 'yes') {
-    # Calculate the end time
-    $endTime = (Get-Date).AddMinutes($scriptDurationInMinutes)
+  # Calculate the end time
+  $endTime = (Get-Date).AddMinutes($scriptDurationInMinutes)
 
-    while((Get-Date) -lt $endTime) {
-        # Generate a random delay between 0.5 and 3 minutes
-        $randomDelayInMinutes = Get-Random -Minimum 0.5 -Maximum 3
+  while ((Get-Date) -lt $endTime) {
+    # Generate a random delay between 0.5 and 3 minutes
+    $randomDelayInMinutes = Get-Random -Minimum 0.5 -Maximum 3
 
-        # Execute the adb command
-        adb shell input swipe 700 2300 700 1000
-
-        # Convert the random delay to milliseconds (1 minute = 60000 milliseconds)
-        $delayInMilliseconds = $randomDelayInMinutes * 60000
-
-        # Wait for the specified delay before repeating the loop
-        Start-Sleep -Milliseconds $delayInMilliseconds
+    # Execute the adb command and check for errors
+    try {
+      adb shell input swipe 700 2300 700 1000
     }
-} else {
-    Write-Host "Please run the script again and enter the correct duration."
+    catch {
+      Write-Host "An error occurred while executing the adb command: $_"
+      break
+    }
+
+    # Convert the random delay to milliseconds (1 minute = 60000 milliseconds)
+    $delayInMilliseconds = $randomDelayInMinutes * 60000
+
+    # Wait for the specified delay before repeating the loop
+    Start-Sleep -Milliseconds $delayInMilliseconds
+  }
+}
+else {
+  Write-Host "Please run the script again and enter the correct duration."
 }
