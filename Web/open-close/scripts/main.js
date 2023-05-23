5302c5b4f9da445a8fff4dfbe8b58151087ca127
vonhyou <moi@rfung.dev>
2023-05-23 16:47:12 -0300

    fix bug: blank line

--- Web/open-close/scripts/main.js
+++ Web/open-close/scripts/main.js
@@ -1,40 +1,55 @@
-var urls = [];
-var interval = 0;
+var urls = []; // Array to hold URLs
+var interval = 0; // Interval between opening URLs
 
+// Function to count and display number of URLs
 function countURLs() {
   var urlInput = document.getElementById('urlInput').value;
-  urls = urlInput.split('\n');
+  urls = urlInput.split('\n').filter(url => url.trim() !== ''); // Split URLs from textarea by newline
 
   var fileInput = document.getElementById('fileInput').files[0];
   if (fileInput) {
     var reader = new FileReader();
     reader.onload = function () {
-      urls = urls.concat(reader.result.split('\n'));
+      // Concatenate URLs from file to existing URLs
+      urls = urls.concat(reader.result.split('\n').filter(url => url.trim() !== ''));
       displayURLCount();
     }
+    reader.onerror = function () {
+      console.error('Failed to read file');
+      reader.abort();
+    };
     reader.readAsText(fileInput);
   } else {
     displayURLCount();
   }
-
-  console.log(urls)
 }
 
+// Function to display number of URLs
 function displayURLCount() {
   document.getElementById('urlCount').innerText = 'URL count: ' + urls.length;
+  console.log(urls); // Log the URLs after they have been read from file or textarea
 }
 
+// Function to generate random interval
 function generateInterval() {
-  interval = Math.floor(Math.random() * 10) + 1;
+  interval = Math.floor(Math.random() * 10) + 1; // Random interval between 1 and 10 seconds
   document.getElementById('interval').innerText = 'Interval: ' + interval + ' seconds';
 }
 
+// Function to open URLs
 function startURLs() {
+  if (urls.length == 0) {
+    alert("No URLs provided. Please enter URLs in the text area or upload a file.");
+    return;
+  }
+
   let i = 0;
   function openNextURL() {
     if (i < urls.length) {
+      // Open the URL in a new tab
       var closeWindow = window.open("https://" + urls[i]);
       setTimeout(function () {
+        // Close the tab after the specified interval
         closeWindow.close();
         i++;
         openNextURL();
