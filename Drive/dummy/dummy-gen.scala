8c7d21fd9bb9c896bb930cb4131fd56c5c46db05
vonhyou <moi@rfung.dev>
2023-06-11 21:39:50 -0300

    update dummy file generator

--- Drive/dummy/dummy-gen.scala
+++ Drive/dummy/dummy-gen.scala
@@ -1,9 +1,11 @@
 import java.nio.ByteBuffer
 import java.nio.file.{Files, Paths, StandardOpenOption}
+import java.time.LocalTime
 
 @main def hello(name: String, number: Int, size: Int): Unit =
   for i <- 0 until(number) do
     createDummyFile(s"$name-$i.zip", size)
+    println(s"[${LocalTime.now}] File `$name-$i.zip' generated successfully")
 
 def createDummyFile(name: String, size: Int) =
   val buffer = ByteBuffer.allocate(1024 * 1024)
